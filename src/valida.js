3dc284ecaa71843cd62174ac3461fd0544130de0
Luis Henrique Vieira Barros <162822478+oluisvi@users.noreply.github.com>
2025-02-19 01:44:59 -0300

    Adicionando no loading ao enviar o formulário

--- src/valida.js
+++ src/valida.js
@@ -1,22 +1,18 @@
-/*Script da verificação do envio de email*/ 
-
 document.getElementById('newsletterForm').addEventListener('submit', function(event) {
-    // Previne o envio do formulário
+    // Previne o envio do formulário 
     event.preventDefault();
-
+  
     // Obtém o valor do input de email
     var email = document.getElementById('emailInput').value;
-
+  
     // Verifica se o email contém "@" e ".com"
     if (email.includes('@') && email.includes('.com')) {
-      // Se o email for válido, permite o envio do formulário
+      
       alert('Email válido! Formulário será enviado.');
-      this.submit();
+      
     } else {
       // Se o email for inválido, exibe uma mensagem de erro
       alert('Por favor, insira um email válido contendo "@" e ".com".');
     }
   });
-
-
-/*Script da verificação de envio do email*/ 
+  
