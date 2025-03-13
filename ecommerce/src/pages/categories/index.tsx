fcf23aa321bb8f3b2bf156f860b2d803ac2b18d5
Luis Henrique Vieira Barros <162822478+oluisvi@users.noreply.github.com>
2025-03-13 09:41:53 -0300

    feat - adicionando página de categorias

--- ecommerce/src/pages/categories/index.tsx
+++ ecommerce/src/pages/categories/index.tsx
@@ -1,7 +1,6 @@
 import BeforeNav from "../../views/header/BeforeNav";
 import Nav from "../../views/header/Nav";
-
-import CustomerRatings from "../../views/homepage/CustomerRatings";
+import MainPageCategory from "../../views/categories/MainPageCategory";
 import Newsletter from "../../views/homepage/Newsletter";
 import Footer from "../../views/footer/Footer";
 import UnderFooter from "../../views/footer/UnderFooter";
@@ -13,8 +12,7 @@ const Categories = () => {
         <>
         <BeforeNav/>
         <Nav/>
-        <TopSelling/>
-        <CustomerRatings/>
+        <MainPageCategory/>
         <Newsletter/>
         <Footer/>
         <UnderFooter/>
